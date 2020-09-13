2b3f31024a2f529d0e78b93a9b2890b2db0cad1e
VanjaRadovanovic <vanja.radovanovich@gmail.com>
2020-09-13 10:43:57 +0200

    Connected to mongoDB cluster

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://localhost/marketplace', {
+mongoose.connect('mongodb+srv://Admin:paco12341@cluster0.jeksz.mongodb.net/Cluster0?retryWrites=true&w=majority', {
   keepAlive: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
